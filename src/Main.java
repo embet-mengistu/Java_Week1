0e96879309da4ef718ec98fc0c2d0a17d0b71a82
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-04-10 21:06:05 -0700

    If statements

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//This line defines a class named Main. In Java, everything must be inside a class.
+//What is main()?It's the entry point of every Java application. When you run your program,
+// Java looks for this method first and starts executing from here
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    //This is a method called go that takes an integer age as a parameter.
+    //  INCLUDE VOID BECAUSE IT WILL NOT RETURN ANYTHING
+    public static void go(int age){
+        if(age >= 18){
+            System.out.println("GO VOTE!!!");
+        }else{
+            System.out.println("GO PLAY");
         }
+    }
+
+    public static void respond(char answer){
+        if(answer == 'Y' || answer == 'y'){
+            System.out.println("YUP YUP YUP");
+        }else{
+            System.out.println("NOPE NOPE NOPE");
+        }
+    }
+
+    public static void drink(int age) {
+        if(age < 21){
+            System.out.println("juice");
+        }else if(age < 50) {
+            System.out.println("adult beverage");
+        }else if(age < 77) {
+            System.out.println("tea");
+        }else{
+            System.out.println("heavy beverage");
+        }
+    }
+   // The main method — this is where the program starts. args is a list of command-line arguments.
+    public static void main(String[] args) {
+        drink(11); // juice
+        drink(25); // adult beverage
+        drink(51); // tea
+        drink(99); // heavy beverage
+
+        for(String a : args) {
+            System.out.println(a);
+        }
+
+        go(13);
+        go(21);
+
+        respond('Y');
+        respond('N');
+
     }
 }
